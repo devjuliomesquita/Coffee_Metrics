69b80cf6d0afa51d8fdd4ce459c6155d209bccd7
juliocesarmcamilo@gmail.com <juliocesarmcamilo@gmail.com>
2025-06-20 18:16:26 -0300

    Retirado adicionado @Column nas records e alterado o application.yml para properties

--- src/main/java/com/juliomesquita/domain/entities/grinding/GrindingIntervalVO.java
+++ src/main/java/com/juliomesquita/domain/entities/grinding/GrindingIntervalVO.java
@@ -1,11 +1,12 @@
 package com.juliomesquita.domain.entities.grinding;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 
 @Embeddable
 public record GrindingIntervalVO(
-    Integer clickInitial,
-    Integer clickFinal
+    @Column(name = "click_initial") Integer clickInitial,
+    @Column(name = "click_final") Integer clickFinal
 ) {
    public static GrindingIntervalVO create(final Integer clickInitial, final Integer clickFinal) {
       return new GrindingIntervalVO(clickInitial, clickFinal);
